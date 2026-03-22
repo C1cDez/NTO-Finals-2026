3d45cf4c10b1821b79e5ff259c6d3b16653b52de
C1cDez <123862840+C1cDez@users.noreply.github.com>
2026-03-22 23:49:53 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,353 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "i2c.h"
+#include "spi.h"
+#include "usart.h"
+#include "gpio.h"
+
+#include "IS_Bluetooth.h"
+#include <string.h>
+#include <Chrono>
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <CC1101.h>
+#include <GyroscopeV2.h>
+#include <MotorFlywheel.h>
+#include <TLV493D.h>
+#include <MagnetometerV2.h>
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+
+#include <Logger.h>
+
+/*void LOGGER(const char *text, uint8_t len)
+{
+    HAL_UART_Transmit(&huart1, (uint8_t *)text, len, 1000);
+}*/
+
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+using namespace IntroSatLib;
+
+/** Создание класса гироскопа*/
+MotorFlyWheel motor(&hi2c1, 0x38);
+GyroscopeV2 gyro(&hi2c1);
+MagnetometerV2 magn1(&hi2c1);
+TLV493D magn2(&hi2c1);
+
+float dw;
+float wz;
+float Integrator = 0;
+float Kp = 400;
+float Ki = 0.3;
+float Kd = 0;
+bool pido = true, rflag = false, gomflag = false;
+float ishowspeed = 0;
+int ztick = 0, ltick = 0;
+
+struct MMU
+{
+	float x1, y1, z1;
+	float x2, y2, z2;
+};
+
+struct MMU mmu_data[360] = {0};
+int mmu_current = 0;
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+
+/** �?нициализация объектов для радио
+IntroSatLib::intefaces::SPI spi(&hspi2);
+IntroSatLib::intefaces::GPIO sck(CC1101_SCK_GPIO_Port, CC1101_SCK_Pin);
+IntroSatLib::intefaces::GPIO mosi(CC1101_MOSI_GPIO_Port, CC1101_MOSI_Pin);
+IntroSatLib::intefaces::GPIO miso(CC1101_MISO_GPIO_Port, CC1101_MISO_Pin);
+IntroSatLib::intefaces::GPIO reset(CC1101_CS_GPIO_Port, CC1101_CS_Pin);
+IntroSatLib::intefaces::GPIO gd0(CC1101_GD0_GPIO_Port, CC1101_GD0_Pin);
+IntroSatLib::CC1101WithGD0 trans(spi, sck, mosi, miso, reset, gd0);
+*/
+
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_I2C1_Init();
+  MX_I2C2_Init();
+  MX_SPI2_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+//  trans.Init();
+//  trans.setCCMode(1);          // set config for internal transmission mode.
+//    trans.setModulation(0);      // set modulation mode. 0 = 2-FSK, 1 = GFSK, 2 = ASK/OOK, 3 = 4-FSK, 4 = MSK.
+//    trans.setMHZ(433.92);        // Here you can set your basic frequency. The lib calculates the frequency automatically (default = 433.92).The cc1101 can: 300-348 MHZ, 387-464MHZ and 779-928MHZ. Read More info from datasheet.
+//    trans.setDeviation(47.60);   // Set the Frequency deviation in kHz. Value from 1.58 to 380.85. Default is 47.60 kHz.
+//    trans.setChannel(0);         // Set the Channelnumber from 0 to 255. Default is cahnnel 0.
+//    trans.setChsp(199.95);       // The channel spacing is multiplied by the channel number CHAN and added to the base frequency in kHz. Value from 25.39 to 405.45. Default is 199.95 kHz.
+//    trans.setRxBW(812.50);       // Set the Receive Bandwidth in kHz. Value from 58.03 to 812.50. Default is 812.50 kHz.
+//    trans.setDRate(1);       // Set the Data Rate in kBaud. Value from 0.02 to 1621.83. Default is 99.97 kBaud!
+//    trans.setPA(5);             // Set TxPower. The following settings are possible depending on the frequency band.  (-30  -20  -15  -10  -6    0    5    7    10   11   12) Default is max!
+//    trans.setSyncMode(2);        // Combined sync-word qualifier mode. 0 = No preamble/sync. 1 = 16 sync word bits detected. 2 = 16/16 sync word bits detected. 3 = 30/32 sync word bits detected. 4 = No preamble/sync, carrier-sense above threshold. 5 = 15/16 + carrier-sense above threshold. 6 = 16/16 + carrier-sense above threshold. 7 = 30/32 + carrier-sense above threshold.
+//    trans.setSyncWord(211, 145); // Set sync word. Must be the same for the transmitter and receiver. (Syncword high, Syncword low)
+//    trans.setAdrChk(0);          // Controls address check configuration of received packages. 0 = No address check. 1 = Address check, no broadcast. 2 = Address check and 0 (0x00) broadcast. 3 = Address check and 0 (0x00) and 255 (0xFF) broadcast.
+//    trans.setAddr(0);            // Address used for packet filtration. Optional broadcast addresses are 0 (0x00) and 255 (0xFF).
+//    trans.setWhiteData(0);       // Turn data whitening on / off. 0 = Whitening off. 1 = Whitening on.
+//    trans.setPktFormat(0);       // Format of RX and TX data. 0 = Normal mode, use FIFOs for RX and TX. 1 = Synchronous serial mode, Data in on GDO0 and data out on either of the GDOx pins. 2 = Random TX mode; sends random data using PN9 generator. Used for test. Works as normal mode, setting 0 (00), in RX. 3 = Asynchronous serial mode, Data in on GDO0 and data out on either of the GDOx pins.
+//    trans.setLengthConfig(1);    // 0 = Fixed packet length mode. 1 = Variable packet length mode. 2 = Infinite packet length mode. 3 = Reserved
+//    trans.setPacketLength(0);    // Indicates the packet length when fixed packet length mode is enabled. If variable packet length mode is used, this value indicates the maximum packet length allowed.
+//    trans.setCrc(0);             // 1 = CRC calculation in TX and CRC check in RX enabled. 0 = CRC disabled for TX and RX.
+//      trans.setCRC_AF(0);          // Enable automatic flush of RX FIFO when CRC is not OK. This requires that only one packet is in the RXIFIFO and that packet length is limited to the RX FIFO size.
+//      trans.setDcFilterOff(0);     // Disable digital DC blocking filter before demodulator. Only for data rates ≤ 250 kBaud The recommended IF frequency changes when the DC blocking is disabled. 1 = Disable (current optimized). 0 = Enable (better sensitivity).
+//      trans.setManchester(0);      // Enables Manchester encoding/decoding. 0 = Disable. 1 = Enable.
+//      trans.setFEC(0);             // Enable Forward Error Correction (FEC) with interleaving for packet payload (Only supported for fixed packet length mode. 0 = Disable. 1 = Enable.
+//      trans.setPRE(0);             // Sets the minimum number of preamble bytes to be transmitted. Values: 0 : 2, 1 : 3, 2 : 4, 3 : 6, 4 : 8, 5 : 12, 6 : 16, 7 : 24
+//      trans.setPQT(0);             // Preamble quality estimator threshold. The preamble quality estimator increases an internal counter by one each time a bit is received that is different from the previous bit, and decreases the counter by 8 each time a bit is received that is the same as the last bit. A threshold of 4∙PQT for this counter is used to gate sync word detection. When PQT=0 a sync word is always accepted.
+//      trans.setAppendStatus(0);    // When enabled, two status bytes will be appended to the payload of the packet. The status bytes contain RSSI and LQI values, as well as CRC OK.
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  char str;
+  motor.Init();
+  gyro.Init();
+  magn1.Init();
+
+  motor.NeedSpeed(0);
+
+  while (1)
+  {
+	  magn1.Read();
+	  magn2.read();
+	  float wx = gyro.X(), wy = gyro.Y(), wz = gyro.Z();
+	  float mx1 = magn1.X(), my1 = magn1.Y(), mz1 = magn1.Z();
+	  float mx2 = magn2.X(), my2 = magn2.Y(), mz2 = magn2.Z();
+
+	  if (!rflag) HAL_UART_Receive(&huart1, (uint8_t*)&str, 1, 1000);
+
+	  if (str == 'b')
+	  {
+	  	  	str = '\n';
+	  	  	HAL_UART_Transmit(&huart1, (uint8_t*)"Bootloader Mode\n\r", 17, 1000);
+	  	  	enter_bootloader();
+	  }
+	  if (str == 'z') {
+	  	    str = '\n';
+	  	    pido = !pido;
+	  	  	motor.NeedSpeed(0);
+	  }
+	  if(pido == true) {
+			Integrator += wz;
+			if (Integrator > 5.0f) { Integrator = 5.0f; }
+			if (Integrator < -5.0f) { Integrator = -5.0f; }
+		    float P = Kp * wz;
+		    float I = Integrator * Ki;
+			float PID_rez = P + I;
+			ishowspeed = motor.CurrentSpeed() + PID_rez;
+
+			if (ishowspeed > 5000) ishowspeed = 5000;
+			if (ishowspeed < -5000) ishowspeed = -5000;
+			motor.NeedSpeed(ishowspeed);
+	  }
+	  //if(str == 'r' || gomflag == true) {
+	  if(HAL_GetTick() - ltick > 20000) {
+		  ltick = HAL_GetTick();
+		  str = '\n';
+		  if(abs(wz) <= 0.25) {
+			  if (ishowspeed >= 0) motor.NeedSpeed(ishowspeed-1250);
+			  else motor.NeedSpeed(ishowspeed+1250);
+			  rflag = true;
+			  pido = false;
+			  gomflag = false;
+		  } else {
+			  pido = true;
+			  gomflag = true;
+		  }
+	  }
+	  if (rflag == true) {
+		  if(HAL_GetTick() - ztick > 10) {
+			  ztick = HAL_GetTick();
+
+			  mmu_data[mmu_current] = {
+					  mx1, my1, mz1, mx2, my2, mz2
+			  };
+			  mmu_current++;
+			  if (mmu_current >= 360)
+			  {
+				  rflag = false;
+				  mmu_current = 0;
+
+				  for (int i = 0; i < 360; i++)
+				  {
+					  char buff[128] = {0};
+					  MMU mmu = mmu_data[i];
+					  int l488 = sprintf(buff, "\n\r%.10f, %.10f, %.10f, %.10f, %.10f, %.10f", mmu.x1, mmu.y1, mmu.z1, mmu.x2, mmu.y2, mmu.z2);
+					  HAL_UART_Transmit(&huart1, (uint8_t*)buff, l488, 1000);
+
+				  }
+
+				  motor.NeedSpeed(ishowspeed);
+				  pido = true;
+			  }
+		  }
+	  }
+	  /*if(HAL_GetTick() - ltick > 1000) {
+		  ltick = HAL_GetTick();
+		  char buf[32] = {0};
+		  int dlen = sprintf(buf, "\n\rpido=%s\n\r", pido ? "True" : "False");
+		  HAL_UART_Transmit(&huart1, (uint8_t*)buf, dlen, 1000);
+	  }*/
+    /* USER CODE END WHILE */
+
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
